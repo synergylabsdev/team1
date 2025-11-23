3e8fdd8a4f0f128ad7d6df19ed3c452bfcb6ef51
Saeed Saydaliyev <saydaleiv@gmail.com>
2025-11-23 14:00:41 +0500

    commit

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,20 +1,21 @@
 class AppConstants {
   // App Info
   static const String appName = 'SampleFinder';
-  
+
   // Supabase Configuration (to be set in environment or config)
-  static const String supabaseUrl = 'YOUR_SUPABASE_URL';
-  static const String supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
-  
+  static const String supabaseUrl = 'https://dplndpkjeqnmnwtrpprg.supabase.co';
+  static const String supabaseAnonKey =
+      'sb_secret_dQDrD6q0lfP3i0srFVOdAQ_tw7XMFjR';
+
   // Shared Preferences Keys
   static const String keyOnboardingCompleted = 'onboarding_completed';
   static const String keyUserSession = 'user_session';
-  
+
   // Validation
   static const int minPasswordLength = 8;
   static const int minUsernameLength = 3;
   static const int maxUsernameLength = 20;
-  
+
   // Points & Tiers
   static const Map<String, int> tierPointsThreshold = {
     'Bronze': 0,
@@ -22,9 +23,8 @@ class AppConstants {
     'Gold': 500,
     'Platinum': 1000,
   };
-  
+
   // Rating
   static const int minRating = 1;
   static const int maxRating = 5;
 }
-
